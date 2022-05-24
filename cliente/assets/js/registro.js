4981e000b7d16b5c676ef042fd032c4c8a3bfc55
Jedicillo <seron.cesar@gmail.com>
2022-05-24 14:07:09 -0400

    Registro no terminado pero devuelve mensaje

---
+++ cliente/assets/js/registro.js
@@ -0,0 +1,56 @@
+const formulario = document.getElementById('formulario');
+const btnRegistro = document.getElementById('btnRegistro');
+const txtEmail = document.getElementById('txtEmail');
+const txtNombre = document.getElementById('txtNombre');
+const txtPass = document.getElementById('txtPass');
+const txtPass2 = document.getElementById('txtPass2');
+const txtExp = document.getElementById('txtExp');
+const txtEspec = document.getElementById('txtEspec');
+const fotoPerfil = document.getElementById('fotoPerfil');
+
+
+btnRegistro.addEventListener('click', async (e) => {
+    //e.preventDefault();
+    console.log('Entra al click del registro');
+    const email = txtEmail.value;
+    const nombre = txtNombre.value;
+    const password = txtPass.value;
+    const experiencia = txtExp.value;
+    const especialidad = txtEspec.value;
+    const foto = fotoPerfil.files[0];
+
+    let registrado = await registrarUsuario(email, nombre, password, experiencia, especialidad, foto);
+    
+
+});
+
+
+const registrarUsuario = (email, nombre, pass, exp, espec, foto) => {
+    const fd = new FormData();
+    fd.append('email', email);
+    fd.append('nombre', nombre);
+    fd.append('pass', pass)
+    fd.append('exp', exp)
+    fd.append('espec', espec)
+    fd.append('foto', foto)
+
+    const registro = fetch("/registro", {
+        method: "POST",
+        //headers: {"Content-Type" : "multipart/form-data"},
+        //headers: {"Content-Type" : "application/json"},
+        //Sin Header para que el navegador agrege el boundary
+        // https://muffinman.io/blog/uploading-files-using-fetch-multipart-form-data/
+        body: fd
+    })
+    .then(res => res.json())
+    .catch(error => console.log('Error: ', error))
+    .then(resp => {
+        console.log('Exito', resp);
+        if(resp){
+            console.log('Registro exitoso', resp.mensaje);
+        }
+        return resp;
+    });
+
+
+};
--- index.js
+++ index.js
@@ -2,25 +2,44 @@ const express = require('express');
 const app = express();
 const { Pool } = require('pg');
 const hbs = require('express-handlebars');
+const fu = require('express-fileupload');
 
 app.listen(3000, () => console.log("Servidor activo en http://localhost:3000"));
 
+app.use(fu());
+app.use(express.json());
+
+app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist`));
+app.use("/css", express.static(`${__dirname}/cliente/assets/css`));
+app.use("/js", express.static(`${__dirname}/cliente/assets/js`));
+app.use("/img", express.static(`${__dirname}/cliente/assets/img`));
+
+
 app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/index.html`);
+    res.sendFile(`${__dirname}/cliente/index.html`);
 });
 
 app.get("/login", (req, res) => {
-    res.sendFile(`${__dirname}/Login.html`);
+    res.sendFile(`${__dirname}/cliente/Login.html`);
 });
 
 app.get("/datos", (req, res) => {
-    res.sendFile(`${__dirname}/Datos.html`);
+    res.sendFile(`${__dirname}/cliente/Datos.html`);
 });
 
 app.get("/admin", (req, res) => {
-    res.sendFile(`${__dirname}/Admin.html`);
+    res.sendFile(`${__dirname}/cliente/Admin.html`);
 });
 
 app.get("/registro", (req, res) => {
-    res.sendFile(`${__dirname}/Registro.html`);
+    res.sendFile(`${__dirname}/cliente/Registro.html`);
+});
+
+app.post("/registro", (req, res) => {
+    const { email, nombre, pass, exp, espec } = req.body;
+    const { foto } = req.files;
+    //const { foto } = req.files;
+    console.log('Registro', {email, nombre, pass, exp, espec});
+    console.log('Registro Foto:', foto.name);
+    res.json({ 'mensaje': 'exito'})
 });
